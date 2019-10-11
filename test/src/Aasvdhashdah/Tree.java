618e288f6df292dce316a767f0d2c7f04e3e4006
Bruno <bdtsa@iscte-iul.pt>
2019-10-11 15:01:26 +0100

    class tree

---
+++ test/src/Aasvdhashdah/Tree.java
@@ -0,0 +1,20 @@
+package Aasvdhashdah;
+
+public class Tree {
+
+	private int height;
+	
+	public Tree(int x) {
+		
+		setHeight(x);
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+}
